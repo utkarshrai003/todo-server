82bf4bc84b28a83d1d80546732053bddbd0bdea8
utkarsh <utkarshrai003@gmail.com>
2017-08-01 16:26:36 +0530

    Adds sign up endpoint to register a user

--- app/routes/userRoute.js
+++ app/routes/userRoute.js
@@ -1,8 +1,35 @@
 
+var passport = require('passport');
+var User = require('../models/user');
+
 module.exports = {
 
-  register: (req, res, next) => {
-    res.send(200).data("hello");
+  signUp: (req, res, next) => {
+    var params = req.body;
+    var email = params.email;
+    var password = params.password;
+    var password_confirmation = params.password_confirmation;
+
+    if(!email || !password)
+      res.send("Please provide both email and password");
+    if(password !== password_confirmation)
+      res.send("Password and password confirmation does not match");
+
+    User.add(email, password)
+    .then((user) => {
+      console.log("I came here bro");
+      res.send(user);
+    })
+    .catch((err) => {
+      res.send(err);
+    })
+  },
+
+  login: (req, res, next) => {
+  },
+
+  googleAuthentication: (req, res, next) => {
+    passport.authenticate('google', {scope: ['profile', 'email']});
   }
 
 }
