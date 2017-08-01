4b9a4499a11d4d55cb75055ec85ef89e2ab1fb4e
utkarsh <utkarshrai003@gmail.com>
2017-08-02 02:32:24 +0530

    Corrects the callback url for google authentication

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
   'googleAuth' : {
     'clientID'      : '823455449184-delfinlsms514cgbdnct7am36ppckpb8.apps.googleusercontent.com',
     'clientSecret'  : 'nuwKV4bs8JwLQAxpu0LPDG3Q',
-    'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+    'callbackURL'   : 'http://localhost:8080/auth/google/callback'
   }
 
 }
