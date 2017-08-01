7a141305d1805290b3c9d12646cb94810cc67c65
utkarsh <utkarshrai003@gmail.com>
2017-08-01 16:28:36 +0530

    Refactored server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const http = require('http');
+const database = require('./config/database');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+
+// specifying port
+var port = process.env.PORT || 8080;
+
+// Creating server
+const app = express();
+const httpServer = http.createServer(app);
+app.listen(port);
+
+// for passport
+app.use(passport.initialize());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+// Import routes
+const user = require('./app/routes/userRoute');
+
+// User routes
+app.get('/auth/google', user.googleAuthentication);
+app.post('/sign_up', user.signUp);
