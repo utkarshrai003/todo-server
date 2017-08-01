895108ecef69a690819d6dd3d33f554821c86018
utkarsh <utkarshrai003@gmail.com>
2017-08-02 02:30:19 +0530

    Adds google authentication strategy to passport.js file

--- app/helpers/passport.js
+++ app/helpers/passport.js
@@ -1,74 +1,79 @@
 
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var GoogleStrategy = require('passport-google-oauth2').Strategy;
+// var JwtStrategy = require('passport-jwt').Strategy;
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-// load up the user model
 var User = require('../models/user');
+var configAuth = require('../../config/auth');
 
-// load the auth variables
-var configAuth = require('./auth');
+module.exports = function(passport) {
 
-module.exports = function() {
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
 
-  // passport.use(new GoogleStrategy ({
-  //   clientId: configAuth.googleAuth.clientID,
-  //   clientSecret: configAuth.googleAuth.clientSecret,
-  //   callbackURL: configAuth.googleAuth.callbackURL,
-  // },
-  // function(token, refreshToken, profile, done) {
-  //   User.findOne({'google.id': profile.id}, function(err, user) {
-  //     if(err)
-  //       return done(err);
-  //
-  //     if(user) {
-  //       return done(null, user);
-  //     }
-  //     else {
-  //       let newUser = new User();
-  //       newUser.google.id = profile.id;
-  //       newUser.google.token = token;
-  //       newUser.google.name = profile.displayName;
-  //       newUser.google.email = profile.emails[0].value;
-  //
-  //       // save the user
-  //       newUser.save(function(err) {
-  //         if(err)
-  //           throw err;
-  //
-  //         return done(null, newUser);
-  //       });
-  //     }
-  //   });
-  // });
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
 
-  passport.use(new TokenStrategy({
-    usernameFiled: 'email',
-    passwordField: 'password',
-    session: false
+  // Passport Google Strategy
+  passport.use(new GoogleStrategy ({
+    clientID: configAuth.googleAuth.clientID,
+    clientSecret: configAuth.googleAuth.clientSecret,
+    callbackURL: configAuth.googleAuth.callbackURL,
   },
-  function(req, email, password, done) {
-    User.findOne({'local.email'}, function(err, user) {
+  function(token, refreshToken, profile, done) {
+    User.findOne({'google.id': profile.id}, function(err, user) {
       if(err)
         return done(err);
 
-        if(user) {
-          return done(null, false);
-        }
-        else {
-          var newUser = new User();
-          newUser.local.email = email;
-          newUser.locl.password = newUser.generateHash(password);
+      if(user) {
+        return done(null, user);
+      }
+      else {
+        let newUser = new User();
+        newUser.google.id = profile.id;
+        newUser.google.token = token;
+        newUser.google.name = profile.displayName;
+        newUser.google.email = profile.emails[0].value;
 
-          newUser.save(function(err) {
-            if(err)
-              throw err;
-            return done(null, newUser);
-          });
-        }
+        // save the user
+        newUser.save(function(err) {
+          console.log("error is here" + err);
+          if(err)
+            throw err;
+
+          return done(null, newUser);
+        });
+      }
     });
   }
-)
+))
 
-
+//   passport.use(new TokenStrategy({
+//     usernameFiled: 'email',
+//     passwordField: 'password',
+//     session: false
+//   },
+//   function(req, email, password, done) {
+//     User.findOne({'local.email'}, function(err, user) {
+//       if(err)
+//         return done(err);
+//
+//         if(user) {
+//           return done(null, false);
+//         }
+//         else {
+//           var newUser = new User();
+//           newUser.local.email = email;
+//           newUser.locl.password = newUser.generateHash(password);
+//
+//           newUser.save(function(err) {
+//             if(err)
+//               throw err;
+//             return done(null, newUser);
+//           });
+//         }
+//     });
+//   }
+// )
 }
