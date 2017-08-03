dccf98682bfb12c07867cddcdae6da03c77ba6c9
utkarsh <utkarshrai003@gmail.com>
2017-08-03 23:09:13 +0530

    Adds auth data for Facebook

--- config/auth.js
+++ config/auth.js
@@ -5,6 +5,11 @@ module.exports = {
     'clientID'      : '823455449184-delfinlsms514cgbdnct7am36ppckpb8.apps.googleusercontent.com',
     'clientSecret'  : 'nuwKV4bs8JwLQAxpu0LPDG3Q',
     'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+  },
+  'facebookAuth' : {
+    'clientID'      : '680696618786956',
+    'clientSecret'  : '6b46809a39918dd9d337740ed3d36099',
+    'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
   }
 
 }
