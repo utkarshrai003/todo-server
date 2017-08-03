309038daf4d2d760cbf0fbba90ca1f94bc7f48ad
utkarsh <utkarshrai003@gmail.com>
2017-08-03 23:10:32 +0530

    Adds path for Facebook authentication

--- server.js
+++ server.js
@@ -28,8 +28,13 @@ const user = require('./app/routes/userRoute');
 app.get('/auth/google', passport.authenticate('google', { session: false, scope: [ 'https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/userinfo.email' ] }));
 app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
-    console.log("I was here 2");
-    res.send();
+    res.send("google authentication successfull");
   });
+app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {failureRedirect : '/login'}),
+      function(req, res) {
+        res.send("facebook authentication successfull");
+      });
 app.post('/sign_up', user.signUp);
 app.post('/login', user.login);
