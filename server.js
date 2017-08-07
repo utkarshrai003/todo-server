f1c380abbb3a30d49a5cb0daeb355563ce53b690
utkarsh <utkarshrai003@gmail.com>
2017-08-08 01:28:01 +0530

    Refactors server.js file

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ const bodyParser = require('body-parser');
 // specifying port
 var port = process.env.PORT || 8080;
 
+// authentication
+const Auth = require('./app/services/auth');
+
 // Creating server
 const app = express();
 const httpServer = http.createServer(app);
 app.listen(port);
 
-require('./app/helpers/passport')(passport);
+require('./app/services/passport')(passport);
 // for passport
 app.use(passport.initialize());
 // parse application/x-www-form-urlencoded
@@ -24,17 +27,22 @@ app.use(bodyParser.json())
 // Import routes
 const user = require('./app/routes/userRoute');
 
-// User routes
-app.get('/auth/google', passport.authenticate('google', { session: false, scope: [ 'https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/userinfo.email' ] }));
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.send("google authentication successfull");
-  });
+// Google Authentication Routes
+app.get('/auth/google', passport.authenticate('google', {
+  session: false,
+  scope: [ 'https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/userinfo.email' ]}
+));
+app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/login'}), user.googleAuthCallback);
+
+// Facebook Authentication Routes
 app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
-app.get('/auth/facebook/callback',
-        passport.authenticate('facebook', {failureRedirect : '/login'}),
-      function(req, res) {
-        res.send("facebook authentication successfull");
-      });
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {failureRedirect : '/login'}), user.facebookAuthCallback);
+
+app.get('/check', Auth.isAuthenticated, function(req, res, next) {
+  console.log("hllo bro");
+  res. send("everytgjkdv is fine");
+})
+
+// Local authentication route
 app.post('/sign_up', user.signUp);
 app.post('/login', user.login);
