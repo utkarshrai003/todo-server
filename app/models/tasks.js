70c1e4752b898af488193e4dcab089efb200cf0e
utkarsh <utkarshrai003@gmail.com>
2017-08-09 19:10:39 +0530

    Adds Task model file

---
+++ app/models/tasks.js
@@ -0,0 +1,33 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+   },
+  due_date: {
+     type: Date
+   },
+  priority: {
+    type: String,
+    enum: ['p1', 'p2', 'p3']
+  },
+  project: {
+    type: Schema.Types.ObjectId,
+    ref: 'Project'
+  },
+  reminder: {
+    isSet: {
+      type: Boolean,
+    },
+    time: {
+      type: Date
+    }
+  },
+  assigned_to: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+})
